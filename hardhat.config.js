06254046b1a912cddd19810dc73ae6fcfb4ce315
Tungneo <66679360+worms12345656@users.noreply.github.com>
2024-08-17 02:26:28 +0700

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  defaultNetwork: "swisstronik",
+  solidity: "0.8.20",
+  networks: {
+    swisstronik: {
+      // If you're using local testnet, replace `url` with local json-rpc address
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`0x` + `${PRIVATE_KEY}`],
+    },
+  },
+};
